3573f292acd50c22aee51611d097725c5b531798
YANGSANGHYUK <134472239+YANGSANGHYUK@users.noreply.github.com>
2023-05-25 15:25:34 +0900

    23.05.25

---
+++ ex02/PT10.java
@@ -0,0 +1,16 @@
+package ex02;
+
+public class PT10 {
+    public static void main(String[] args) {
+        double pizza1 = 3.141592 * 20 * 20 *2;
+        double pizza2 = 3.141592 * 30 * 30;
+        System.out.println(pizza1);
+        System.out.println(pizza2);
+        System.out.println((pizza1 > pizza2)? "20cm 두 개": "30cm 한 개");
+       
+        
+
+        
+    }
+    
+}
